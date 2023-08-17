2cde2410c652e7b3f234ae6a071d8796a6010830
Bryan Vernon <bryan.vernon@hotmail.com>
2023-08-17 09:04:03 -0500

    Add files via upload

---
+++ ProfilePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ProfilePage.css';
+
+const ProfilePage = ({ userData }) => {
+  return (
+    <div className="profile-page">
+      {/* Display user's profile information here */}
+    </div>
+  );
+};
+
+export default ProfilePage;
